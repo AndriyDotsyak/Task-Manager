ddce8f56492c6d3a5ee86149a4ba4388da0b2ace
Andriy <andriy.dotsyak7@gmail.com>
2018-01-17 16:49:08 +0200

    # Read EditText

--- Stage 1/TaskManager/app/src/main/java/manager/taskmanager/task/CreatingTask.java
+++ Stage 1/TaskManager/app/src/main/java/manager/taskmanager/task/CreatingTask.java
@@ -2,14 +2,59 @@ package manager.taskmanager.task;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
 
 import manager.taskmanager.R;
 
-public class CreatingTask extends AppCompatActivity {
+public class CreatingTask extends AppCompatActivity implements View.OnClickListener, View.OnKeyListener {
+    EditText etDenomination;
+    EditText etComment;
+
+    private int pressedEnter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creating_task);
+
+        etDenomination = findViewById(R.id.et_Denomination);
+        etComment = findViewById(R.id.et_Comment);
+
+        etDenomination.setOnKeyListener(this);
+        etComment.setOnKeyListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_Save:
+                Log.d("main", "Save");
+                break;
+            case R.id.btn_Exit:
+                Log.d("main", "Exit");
+                break;
+        }
+    }
+
+    @Override
+    public boolean onKey(View v, int keyCode, KeyEvent event) {
+        switch (v.getId()) {
+            case R.id.et_Denomination:
+                break;
+            case R.id.et_Comment:
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    pressedEnter++;
+                    Log.d("main", String.valueOf(pressedEnter));
+                    if (pressedEnter == 2) {
+                        etComment.setLongClickable(false);
+                        return true;
+                    }
+                }
+                break;
+        }
+        return false;
     }
 }
--- Stage 1/TaskManager/app/src/main/java/manager/taskmanager/task/MainActivity.java
+++ Stage 1/TaskManager/app/src/main/java/manager/taskmanager/task/MainActivity.java
@@ -1,15 +1,33 @@
 package manager.taskmanager.task;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import manager.taskmanager.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button btnAddTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAddTask = findViewById(R.id.btn_AddTask);
+        btnAddTask.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_AddTask:
+                Intent intent = new Intent("android.intent.creating.task");
+                startActivity(intent);
+                break;
+        }
     }
 }
